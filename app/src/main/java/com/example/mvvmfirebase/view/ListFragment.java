7b4c267044ed96a32f661a852f0687b7f94e7745
sifat hassan <sifat007.sh7@gmail.com>
2021-06-07 20:24:08 +0600

    setup repository and viewModel

--- app/src/main/java/com/example/mvvmfirebase/view/ListFragment.java
+++ app/src/main/java/com/example/mvvmfirebase/view/ListFragment.java
@@ -1,15 +1,30 @@
 package com.example.mvvmfirebase.view;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mvvmfirebase.R;
+import com.example.mvvmfirebase.model.ContactUser;
+import com.example.mvvmfirebase.viewmodel.ContactViewModel;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
 public class ListFragment extends Fragment {
+
+    //view Model
+    private ContactViewModel contactViewModel;
 
 
     @Override
@@ -18,4 +33,29 @@ public class ListFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViewModel();
+        setUpRecycle();
+    }
+
+    private void setUpRecycle() {
+        final AlertDialog dialogue= new SpotsDialog.Builder().setContext(getActivity()).setTheme(R.style.Custom).setCancelable(true).build();
+        dialogue.show();
+        contactViewModel.show();
+        contactViewModel.getContactLiveData.observe(getViewLifecycleOwner(), new Observer<List<ContactUser>>() {
+            @Override
+            public void onChanged(List<ContactUser> contactUsers) {
+                dialogue.dismiss();
+            }
+        });
+    }
+
+    private void initViewModel() {
+        contactViewModel= new ViewModelProvider(getActivity(),ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()))
+                .get(ContactViewModel.class);
+    }
+
 }
--- app/src/main/java/com/example/mvvmfirebase/viewmodel/ContactViewModel.java
+++ app/src/main/java/com/example/mvvmfirebase/viewmodel/ContactViewModel.java
@@ -10,11 +10,14 @@ import androidx.lifecycle.LiveData;
 import com.example.mvvmfirebase.model.ContactUser;
 import com.example.mvvmfirebase.repository.ContactRepository;
 
+import java.util.List;
+
 public class ContactViewModel extends AndroidViewModel {
 
 
     private ContactRepository repository;
     public LiveData<String> insertResultLiveData;
+    public LiveData<List<ContactUser>> getContactLiveData;
 
     public ContactViewModel(@NonNull Application application) {
         super(application);
@@ -23,5 +26,9 @@ public class ContactViewModel extends AndroidViewModel {
 
     public void insert(ContactUser user, Uri uri){
         insertResultLiveData= repository.insertContactFirebase(user, uri);
+    }
+
+    public void show(){
+        getContactLiveData= repository.getDataFromFireStore();
     }
 }
