7a9afc3c4671f70209adea729d42294fd94a6805
sifat hassan <sifat007.sh7@gmail.com>
2021-06-07 20:45:39 +0600

    data show in recycler view

--- app/src/main/java/com/example/mvvmfirebase/view/ListFragment.java
+++ app/src/main/java/com/example/mvvmfirebase/view/ListFragment.java
@@ -5,24 +5,33 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mvvmfirebase.R;
+import com.example.mvvmfirebase.adapter.ContactAdapter;
 import com.example.mvvmfirebase.model.ContactUser;
 import com.example.mvvmfirebase.viewmodel.ContactViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import dmax.dialog.SpotsDialog;
 
 public class ListFragment extends Fragment {
 
+    private SearchView searchView;
+    private RecyclerView recyclerView;
+    private ContactAdapter adapter;
+    private List<ContactUser> userList= new ArrayList<>();
     //view Model
     private ContactViewModel contactViewModel;
 
@@ -39,6 +48,13 @@ public class ListFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initViewModel();
         setUpRecycle();
+
+        //find Section...
+        searchView= view.findViewById(R.id.searchViewId);
+        recyclerView= view.findViewById(R.id.recycleViewId);
+        adapter= new ContactAdapter();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
     private void setUpRecycle() {
@@ -49,6 +65,9 @@ public class ListFragment extends Fragment {
             @Override
             public void onChanged(List<ContactUser> contactUsers) {
                 dialogue.dismiss();
+                userList= contactUsers;
+                adapter.getContactList(userList);
+                recyclerView.setAdapter(adapter);
             }
         });
     }
